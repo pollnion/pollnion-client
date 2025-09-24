2233869dae94da0c44c2b3db9a6c751a139b18ac
Charlito Jr Mecarez <carenzdelarosa@gmail.com>
2025-09-24 12:36:40 +0800

    refactor: layouts and add sign-in/out pages
    Reworked global styles for improved theming and dark mode support. Updated app layout to use AppProviders and suppress hydration warning. Refactored home page to use PublicLayout and Landing module. Added new sign-in and sign-out pages with consistent layout structure.

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignIn from '@/modules/sign-in'
+import PublicLayout from '@/components/layouts/public-layout'
+
+const page = () => (
+  <PublicLayout>
+    <SignIn />
+  </PublicLayout>
+)
+
+export default page
