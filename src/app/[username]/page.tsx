0f508618bb394a575e72fc1d2aa5feecf860fc48
javaplumdev <carenzdelarosa@gmail.com>
2025-11-27 08:59:39 +0800

    feat: updated queries in profile and posts

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -4,6 +4,7 @@ import isEmpty from "lodash/isEmpty";
 import { FeedItem } from "@/models";
 import Profile from "@/modules/profile";
 import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { ProfileItem } from "@/models/profiles";
 import { TABLE_FEED } from "@/constants/tables";
 import FeedCard from "@/modules/feed/feed-card";
@@ -13,14 +14,23 @@ import Breadcrumb from "@/components/custom/breadcrumbs";
 import ProfileVirtuoso from "@/components/custom/virtusio";
 import { useInfiniteQuery, useReadStoreById } from "@/store";
 import { Typography } from "@/components/custom/typography";
-import { useRouter } from "next/router";
 
 const Page = () => {
   const params = useParams();
   const router = useRouter();
   const username = params.username as string;
-  const listProps = useInfiniteQuery<FeedItem>({ tableName: TABLE_FEED });
-  const viewProps = useReadStoreById<ProfileItem>("profiles", { username });
+  const listProps = useInfiniteQuery<FeedItem>({
+    tableName: TABLE_FEED,
+    trailingQuery: (query) =>
+      query
+        .eq("author->>name", username)
+        .eq("author->>name", username.toLowerCase())
+        .order("created_at", { ascending: false }),
+  });
+
+  const viewProps = useReadStoreById<ProfileItem>("profiles", {
+    filters: { username },
+  } as Partial<ProfileItem>);
 
   const data = viewProps?.data as ProfileItem;
   const isLoading = viewProps?.isLoading || listProps?.isLoading;
